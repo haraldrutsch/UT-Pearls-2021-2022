4482f254bf0bbb7952d10bd2eb9508fddacda360
MChecaH <marti@checa.cat>
2021-11-02 14:53:26 +0100

    Parser with text output (Fully parsed in 14 minutes)

--- parser.py
+++ parser.py
@@ -7,3 +7,5 @@ file = urllib.request.urlopen(url)
 for line in file:
     tweetAux = json.loads(line)
     print(tweetAux['created_at'], "-", tweetAux['text'].encode('utf-8'), file=open('output.txt', 'a'))
+
+print('done')
