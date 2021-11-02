ffa0919256e9f9a6a4e76e1aee522fbd82b8993d
MChecaH <marti@checa.cat>
2021-11-02 14:25:18 +0100

    Parser with text output

---
+++ parser.py
@@ -0,0 +1,9 @@
+import json
+import urllib.request
+
+url = "https://bronto.ewi.utwente.nl/ecadata/sports-20191117.txt"
+file = urllib.request.urlopen(url)
+
+for line in file:
+    tweetAux = json.loads(line)
+    print(tweetAux['created_at'], "-", tweetAux['text'].encode('utf-8'), file=open('output.txt', 'a'))
