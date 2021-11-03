c581408b2f9b4b58eb3b8e4e07980b072bfbc9c0
Titas Lukaitis <Traydr@protonmail.com>
2021-11-03 15:54:00 +0100

    WIP counter_for_graph func

--- counter.py
+++ counter.py
@@ -7,6 +7,7 @@ sample_time_date_start = "Sat Nov 16 13:52:20 +0000 2019"
 sample_time_date_end = "Sun Nov 17 04:38:18 +0000 2019"
 
 
+# Think how this could be put into a stream and not read from file
 def list_maker(input):
     with open(input, encoding='utf8') as in_file:
         all_lines = in_file.readlines()
@@ -14,7 +15,7 @@ def list_maker(input):
     results = []
     for line in all_lines:
         parts = line.split(split_by_chars)
-        parts[0] = convert_time_date_to_unix_millis(parts[0])
+        parts[0] = convert_time_date_to_unix(parts[0])
         parts.append(filter_by_sport(parts[1]))
         results.append(parts)
     input = []
@@ -23,7 +24,37 @@ def list_maker(input):
     return input
 
 
-def convert_time_date_to_unix_millis(input):
+def counter_for_graph(input_list, time_frame_unix):
+    start_unix_time = input_list[0][0]
+    end_unix_time = start_unix_time + time_frame_unix
+
+    unix_time_frames = []
+    baseball_totals = []
+    basketball_totals = []
+    volleyball_totals = []
+    tennis_totals = []
+    cricket_totals = []
+    soccer_totals = []
+    football_totals = []
+    rugby_totals = []
+
+    unix_time_frames.append(end_unix_time)
+    time_frame_index = 0
+
+    for tweet in input_list:
+        if tweet[2] == []:
+            continue
+        elif tweet[0] <= unix_time_frames[time_frame_index]:
+            print()
+        else:
+            unix_time_frames.append(unix_time_frames[time_frame_index] + time_frame_unix)
+            time_frame_index += 1
+
+    all_totals = [unix_time_frames, baseball_totals, basketball_totals, volleyball_totals, tennis_totals, cricket_totals, soccer_totals, football_totals, rugby_totals]
+    return all_totals
+
+
+def convert_time_date_to_unix(input):
     # Assuming date time comes in form -> Sun Nov 17 04:38:18 +0000 2019
     split_input = re.split(" ", input)
     date_time_filtered = "{1}/{0}/{2}, {3}".format(covert_string_month_to_num(split_input[1]), split_input[2], split_input[5], split_input[3])
@@ -32,11 +63,12 @@ def convert_time_date_to_unix_millis(input):
     return unix_time
 
 
-def convert_unix_millis_to_time_date(input):
+def convert_unix_to_time_date(input):
     print()
     # TODO: make this func
     # datetime.datetime.fromtimestamp(ms/1000.0)
     # to convert back into date time
+
 
 def covert_string_month_to_num(input_mon):
     if input_mon.lower() == "jan":
