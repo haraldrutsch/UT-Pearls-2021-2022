cfbc61aa6f609d9fb9aa7c4520fa57cec6e699fe
haraldrutsch <harald.rutsch@gmai.com>
2021-11-04 12:04:09 +0100

    Changed Main to new

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from filter_tweet import *
-from counter import *
+import filter_tweet
+import counter
+import parser
+#Main is going to be our Program.
 
-filtered_data = 'output.txt'
-
-
-def main():
-    print("")
-    print(counter_for_graph(list_maker(Sat Nov 16 12:51:41 +0000 2019 - b'@BobGreenburg @ONeill_Coffee Congrats to the excellent football program at Wilmington. One classy organization!'), 4 ))
-
-
-if __name__ == '__main__':
-    main()
