320e3120c14c6e059d75a25ca794c09e43d5bc35
haraldrutsch <harald.rutsch@gmai.com>
2021-11-04 14:19:00 +0100

    Changed Main to new

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import counter
 from parser import *
 #Main is going to be our Program.
 url = "https://bronto.ewi.utwente.nl/ecadata/sports-20191117.txt"
-file = urllib.request.urlopen(url)
-time_interval = 10
+time_interval = 10*60 #in seconds
+first_tweet
 
-new_data = parser(file, time_interval)
+new_data = []
+new_data = parser(url, time_interval)
+
+cleaned_data
