41086639c3baffcf043fe1a1787cbe5e7b34aa8c
Machiel20 <57542320+Machiel20@users.noreply.github.com>
2021-11-04 14:20:25 +0100

    parser redo

--- parser.py
+++ parser.py
@@ -1,12 +1,38 @@
 import json
 import urllib.request
+from counter import *
 
-url = "https://bronto.ewi.utwente.nl/ecadata/sports-20191117.txt"
-file = urllib.request.urlopen(url)
+first_tweet_time = 	1573905101
+end_time = 0
+itteration = 0
 
-# Takes useful data from the .json file and saves result in output.txt
-for line in file:
-    tweetAux = json.loads(line)
-    print(tweetAux['created_at'], "-", tweetAux['text'].encode('utf-8'), file=open('output.txt', 'a'))
 
-print('done')
+
+def parser(url, time_frame):
+    file = urllib.request.urlopen(url)
+
+    global first_tweet_time
+    global itteration
+    global end_time
+
+    cleaned_data = []
+
+    if itteration == 0:
+        tweet_time = first_tweet_time
+    else:
+        tweet_time = end_time
+
+    for line in file:
+        tweetAux = json.loads(line)
+        if  convert_time_date_to_unix(tweetAux['created_at']) <= tweet_time + time_frame:
+            input_data = convert_time_date_to_unix(tweetAux['created_at']), tweetAux['text'].encode('utf-8')
+            cleaned_data.append(input_data)
+            end_time = convert_time_date_to_unix(tweetAux['created_at'])
+        else:
+            break
+    
+    itteration += 1
+
+    return cleaned_data
+
+    
