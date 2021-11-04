ad6908cd8510cee787905f2caca1ad35e1d533d7
haraldrutsch <harald.rutsch@gmai.com>
2021-11-04 15:15:02 +0100

    Changed Main to new

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import filter_tweet
-import counter
-from parser import *
+import parserUT
+
 #Main is going to be our Program.
 url = "https://bronto.ewi.utwente.nl/ecadata/sports-20191117.txt"
 time_interval = 10*60 #in seconds
 first_time = 1573905101
-
-first_tweet
-
 new_data = []
-new_data = parserI(url, time_interval,)
-
-cleaned_data
+new_data = parserUT.parserI(url, time_interval, first_time)
+print(new_data)
