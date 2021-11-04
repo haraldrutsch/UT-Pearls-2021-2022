5d5b4a557e5563643cd17bc7f77d5b44075163a2
haraldrutsch <harald.rutsch@gmai.com>
2021-11-04 12:19:34 +0100

    Changed Main to new

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import filter_tweet
 import counter
-import parser
+from parser import *
 #Main is going to be our Program.
+url = "https://bronto.ewi.utwente.nl/ecadata/sports-20191117.txt"
+file = urllib.request.urlopen(url)
+time_interval = 10
 
+new_data = parser(file, time_interval)
