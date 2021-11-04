f33ca7d627b459a40144298b940ac23985393b8d
Titas Lukaitis <Traydr@protonmail.com>
2021-11-04 23:19:07 +0100

    new front end design

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ from dash import dcc
 import pandas
 import plotly.express as px
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets) 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 labels = ['Baseball', 'Basketball', 'Voleyball', 'Tennis', 'Cricket', 'Soccer', 'Football', 'Rugby']
 values = [4500, 2500, 1053, 500, 200, 300, 150, 2000]
 
 title = 'Twente Analytica'
 labels = ['Baseball', 'Basketball', 'Voleyball', 'Tennis', 'Cricket', 'Soccer', 'Football', 'Rugby']
-colors = ['rgb(67,67,67)', 'rgb(115,115,115)', 'rgb(49,130,189)', 'rgb(189,189,189)', 'rgb(255,192,203)', 'rgb(255,0,0)', 'rgb(255, 165,0)', 'rgb(0,128,0)']
+colors = ['rgb(67,67,67)', 'rgb(115,115,115)', 'rgb(49,130,189)', 'rgb(189,189,189)', 'rgb(255,192,203)',
+          'rgb(255,0,0)', 'rgb(255, 165,0)', 'rgb(0,128,0)']
 
 mode_size = [8, 8, 8, 8, 8, 8, 8, 8]
 line_size = [2, 2, 2, 2, 2, 2, 2, 2]
 
-x_data = np.vstack((np.arange(2001, 2014),)*8)
+x_data = np.vstack((np.arange(2001, 2014),) * 8)
 
 y_data = np.array([
     [74, 82, 80, 74, 73, 72, 74, 70, 70, 66, 66, 100],
@@ -32,25 +32,25 @@ y_data = np.array([
     [10, 19, 20, 21, 16, 14, 11, 18, 17, 20, 39, 25],
 ])
 
-#Pie chart
+# Pie chart
 fig1 = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent',
-                             insidetextorientation='radial'
-                            )])
-#Line chart
+                              insidetextorientation='radial'
+                              )])
+# Line chart
 fig = go.Figure()
 for i in range(0, 8):
     fig.add_trace(go.Scatter(x=x_data[i], y=y_data[i], mode='lines',
-        name=labels[i],
-        line=dict(color=colors[i], width=line_size[i]),
-        connectgaps=True,
-    ))
+                             name=labels[i],
+                             line=dict(color=colors[i], width=line_size[i]),
+                             connectgaps=True,
+                             ))
     # endpoints
-    #fig.add_trace(go.Scatter(
+    # fig.add_trace(go.Scatter(
     #    x=[x_data[i][0], x_data[i][-1]],
     #    y=[y_data[i][0], y_data[i][-1]],
     #    mode='markers',
     #    marker=dict(color=colors[i], size=mode_size[i])
-    #))
+    # ))
 
 fig.update_layout(
     xaxis=dict(
@@ -67,7 +67,7 @@ fig.update_layout(
         ),
     ),
     yaxis=dict(
-        showgrid= False,
+        showgrid=False,
         zeroline=False,
         showline=True,
         showticklabels=True,
@@ -77,80 +77,80 @@ fig.update_layout(
         tickfont=dict(
             family='Arial',
             size=12,
-            color='rgb(82, 82, 82)',)
+            color='rgb(82, 82, 82)', )
 
-    ),)
-    #autosize=False,
-    #margin=dict(
-    #    autoexpand=True
-    #    l=100,
-    #    r=20,
-    #    t=110,
-    #),
-    #showlegend=True,
-    #plot_bgcolor='white'
-#)
+    ), )
+# autosize=False,
+# margin=dict(
+#    autoexpand=True
+#    l=100,
+#    r=20,
+#    t=110,
+# ),
+# showlegend=True,
+# plot_bgcolor='white'
+# )
 
 annotations = []
 
 # Adding labels
-#for y_trace, label, color in zip(y_data, labels, colors):
-    # labeling the left_side of the plot
+# for y_trace, label, color in zip(y_data, labels, colors):
+# labeling the left_side of the plot
 #    annotations.append(dict(xref='paper', x=0.05, y=y_trace[0],
 #                                  xanchor='right', yanchor='middle',
 #                                  showarrow=True))
-    # labeling the right_side of the plot
+# labeling the right_side of the plot
 
 fig.update_layout(annotations=annotations)
 
-app.layout =html.Div([
-    html.Div(style = {
-    'backgroundColor': '#FFFFFF'}, 
-    children = [
-        html.H1(
-        children = 'Twente Analytica',
-        style = {
-        'textAlign': 'center',
-        'color': '#000000'}),
-    html.Div([
-        html.H3(
-        children = 'Line Chart',
-        style = {
-        'textAlign': 'center',
-        'color': '#000000',
-        'font': 'Times New Roman'}),
-            dcc.Graph(
-                id = 'example-graph-2',
-                figure = fig,
-                style = {
-                'height': 500,
-                'width': 750,
-                "display": "inline-block",
-                "margin-top": "auto",
-                "margin-left": "",
-                "margin-right": ""
-                }),
-    ], className= "seven columns"),
-    html.Div([
-        html.H3(
-        children = 'Popularity',
-        style = {
-        'textAlign': 'center',
-        'color': '#000000'}),
-            dcc.Graph(
-                id='pie1',
-                figure= fig1,
-                style = {
-                'height': 500,
-                'width': 600,
-                "display": "",
-                "margin-left": "",
-                "margin-right": ""
-                },),
-        ], className= "seven columns"),
-    ], className= "container")
+app.layout = html.Div([
+    html.Div(style={
+        'backgroundColor': '#FFFFFF'},
+        children=[
+            html.H1(
+                children='Twente Analytica',
+                style={
+                    'textAlign': 'center',
+                    'color': '#000000'}),
+            html.Div([
+                html.H3(
+                    children='Line Chart',
+                    style={
+                        'textAlign': 'center',
+                        'color': '#000000',
+                        'font': 'Times New Roman'}),
+                dcc.Graph(
+                    id='example-graph-2',
+                    figure=fig,
+                    style={
+                        'height': 500,
+                        'width': 750,
+                        "display": "inline-block",
+                        "margin-top": "auto",
+                        "margin-left": "",
+                        "margin-right": ""
+                    }),
+            ], className="seven columns"),
+            html.Div([
+                html.H3(
+                    children='Popularity',
+                    style={
+                        'textAlign': 'center',
+                        'color': '#000000'}),
+                dcc.Graph(
+                    id='pie1',
+                    figure=fig1,
+                    style={
+                        'height': 500,
+                        'width': 600,
+                        "display": "",
+                        "margin-left": "",
+                        "margin-right": ""
+                    }, ),
+            ], className="seven columns"),
+        ], className="container")
 ])
 if __name__ == '__main__':
-  app.run_server(debug = True)
+    app.run_server(debug=True)
 fig.show()
 fig1.show()
---
+++ plotly_web_app_dog.py
@@ -0,0 +1,144 @@
+import datetime
+
+import dash
+from dash import html
+from dash import dcc
+import plotly
+from dash.dependencies import Input, Output
+from parserUT import *
+import pandas
+import plotly.graph_objects as go
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# Global variables
+line_chart_data = {
+    'time': [],
+    'baseball': [],
+    'basketball': [],
+    'volleyball': [],
+    'tennis': [],
+    'cricket': [],
+    'soccer': [],
+    'football': [],
+    'rugby': []
+}
+
+last_time_frame_index = 0
+data_url = "https://bronto.ewi.utwente.nl/ecadata/sports-20191117.txt"
+time_interval = 10 * 60
+labels = ['Baseball', 'Basketball', 'Voleyball', 'Tennis', 'Cricket', 'Soccer', 'Football', 'Rugby']
+colors = ['rgb(67,67,67)', 'rgb(115,115,115)', 'rgb(49,130,189)', 'rgb(189,189,189)', 'rgb(255,192,203)',
+          'rgb(255,0,0)', 'rgb(255, 165,0)', 'rgb(0,128,0)']
+mode_size = [8, 8, 8, 8, 8, 8, 8, 8]
+line_size = [2, 2, 2, 2, 2, 2, 2, 2]
+# Global variables
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    html.Div(style={
+        'backgroundColor': '#FFFFFF'},
+        children=[
+            html.H1(
+                children='Twente Analytica',
+                style={
+                    'textAlign': 'center',
+                    'color': '#000000'}),
+            html.Div([
+                html.H3(
+                    children='Line Chart',
+                    style={
+                        'textAlign': 'center',
+                        'color': '#000000',
+                        'font': 'Times New Roman'}),
+                dcc.Graph(
+                    id='line_chart_main',
+                    figure=fig,
+                    style={
+                        'height': 500,
+                        'width': 750,
+                        "display": "inline-block",
+                        "margin-top": "auto",
+                        "margin-left": "",
+                        "margin-right": ""
+                    }),
+                dcc.Interval(
+                    id='interval_line_chart_main',
+                    interval=1 * 1000,  # in milliseconds
+                    n_intervals=0
+                ),
+            ], className="seven columns"),
+            html.Div([
+                html.H3(
+                    children='Popularity',
+                    style={
+                        'textAlign': 'center',
+                        'color': '#000000'}),
+                dcc.Graph(
+                    id='pie_chart_main',
+                    figure=fig1,
+                    style={
+                        'height': 500,
+                        'width': 600,
+                        "display": "",
+                        "margin-left": "",
+                        "margin-right": ""
+                    }, ),
+                dcc.Interval(
+                    id='interval_pie_chart_main',
+                    interval=1 * 1000,  # in milliseconds
+                    n_intervals=0
+                ),
+            ], className="seven columns"),
+        ], className="container")
+])
+
+
+# Multiple components can update everytime interval gets fired.
+@app.callback(Output('line_chart_main', 'figure'),
+              Input('interval_line_chart_main', 'n_intervals'))
+def update_graph_live(n):
+    global line_chart_data
+    global last_time_frame_index
+    global data_url
+    global time_interval
+    global labels
+    global colors
+    global mode_size
+    global line_size
+
+    # Collect some data
+    temp = parser(data_url, time_interval, last_time_frame_index)
+    last_time_frame_index += 1
+
+    line_chart_data['time'].append(temp[0][0])
+    line_chart_data['baseball'].append(temp[1][0])
+    line_chart_data['basketball'].append(temp[2][0])
+    line_chart_data['volleyball'].append(temp[3][0])
+    line_chart_data['tennis'].append(temp[4][0])
+    line_chart_data['cricket'].append(temp[5][0])
+    line_chart_data['soccer'].append(temp[6][0])
+    line_chart_data['football'].append(temp[7][0])
+    line_chart_data['rugby'].append(temp[8][0])
+
+    # Create the graph with subplots
+    fig = plotly.tools.make_subplots(rows=1, cols=1, vertical_spacing=0.2)
+    fig['layout']['margin'] = {
+        'l': 30, 'r': 10, 'b': 30, 't': 10
+    }
+    fig['layout']['legend'] = {'x': 0, 'y': 1, 'xanchor': 'left'}
+
+    for n in range(1, 8):
+        fig.append_trace(go.Scatter(x=line_chart_data['time'][last_time_frame_index],
+                                    y=line_chart_data[n][last_time_frame_index],
+                                    mode='lines',
+                                    line=dict(color=colors[n], width=line_size[n]),
+                                    connectgaps=True))
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
