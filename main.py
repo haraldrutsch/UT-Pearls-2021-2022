7026201946a78f2354b40b710e3ca15658347bf4
haraldrutsch <harald.rutsch@gmai.com>
2021-11-04 15:32:57 +0100

    Changed Main to new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import parserUT
+import counter
 
 #Main is going to be our Program.
 url = "https://bronto.ewi.utwente.nl/ecadata/sports-20191117.txt"
@@ -6,4 +7,5 @@ time_interval = 10*60 #in seconds
 first_time = 1573905101
 new_data = []
 new_data = parserUT.parserI(url, time_interval, first_time)
-print(new_data)
+
+print(counter.counter_for_graph(new_data,time_interval))
